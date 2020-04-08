d00e1da762a767d26ec9e695e33a80e0c9d1bc4a
Woo yong Kim <48975983+LunaALL@users.noreply.github.com>
2020-04-08 17:13:10 +0900

    try  catch
    dd

---
+++ JavaGG                   ./DivideByZero.java
@@ -0,0 +1,41 @@
+package chp18;
+
+import java.util.Scanner;
+
+public class DivideByZero {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*
+		System.out.println("두 개의 정수 입력");
+		Scanner sc = new Scanner(System.in);
+		int num1=sc.nextInt();
+		int num2=sc.nextInt();
+		
+		try {
+			System.out.println("나눗셈 결과 목 = "+(num1/num2));
+			System.out.println("나눗셈 결과의 나머지 : " + (num1%num2));
+		}
+		catch(ArithmeticException e) {
+			System.out.println("나눗셈 불가");
+			System.out.println(e.getMessage());
+			
+		} */
+	String str1= "ff";
+	String str2= "g1q223";
+	
+	if(str1.equals(str2) ) {
+		System.out.println("같은 스트링");
+	} else
+		System.out.println("다른거");
+		
+	
+	
+	
+
+	}
+	
+	
+
+}
