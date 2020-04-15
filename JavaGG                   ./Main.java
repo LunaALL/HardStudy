43fc5a20bbb10ddb81d72a53e4ee2138116237e5
Woo yong Kim <48975983+LunaALL@users.noreply.github.com>
2020-04-15 11:35:36 +0900

    unicode link
    유니코드 연결해서 이쁜 사각형

---
+++ JavaGG                   ./Main.java
@@ -0,0 +1,13 @@
+package Basic;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=10;
+     System.out.printf("\u250C%d\u252C%d\u2510\n",n,n);
+     System.out.printf("\u251C%d\u253C%d\u2524\n",n,n);
+     System.out.printf("\u2514\u2534\u2518");
+	}
+
+}
